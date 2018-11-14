f893aecadec62218fc0d3aa5d4bad74e3fa681db
sameerajonnalagadda <35942980+sameerajonnalagadda@users.noreply.github.com>
2018-11-14 16:36:36 -0600

    Add files via upload
    Tkinter demo

---
+++ tkinter/kg_to_other.py
@@ -0,0 +1,35 @@
+from tkinter import *
+
+window = Tk()
+
+def convert():
+    grams = int(e1_value.get())* 1000
+    pounds = int(e1_value.get())*2.20
+    ounces = int(e1_value.get())*35.2
+    t1.insert(END, grams)
+    t2.insert(END, pounds)
+    t3.insert(END, ounces)
+
+
+l1 = Label(window, text = 'KG')
+l1.grid(row = 0, column = 0)
+
+e1_value = StringVar()
+e1 = Entry(window, textvariable = e1_value)
+e1.grid(row = 0, column = 1)
+
+b1 = Button(window, text = 'Convert', command = convert)
+b1.grid(row = 0, column = 2)
+
+t1 = Text(window, height = 1, width = 20)
+t1.grid(row = 1, column = 0)
+
+t2 = Text(window, height = 1, width = 20)
+t2.grid(row = 1, column = 2)
+
+t3 = Text(window, height = 1, width = 20)
+t3.grid(row = 1, column = 3)
+
+
+
+window.mainloop()
